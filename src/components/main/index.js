f468810a18b0784e9a4935b746843077ad7162ee
Agustin De Robles <agustin.derobles1995@gmail.com>
2023-07-15 14:55:46 -0300

    Login and register finish

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,22 +1,63 @@
-import { useContext } from "react";
-import { DivForm, FormContainer, FormWrapper, Input, Label, MainWrapper } from "./styles";
+import { useContext, useState } from "react";
+import { DivForm, FormRegisterContainer, FormWrapper, Input, Label, OptionsContainer, OptionsSelect, MainWrapper, FormLoginContariner, ButtonSendForm, P } from "./styles";
 import ShowElements from "../../context/showElements";
+import InfoLogin from "../../context/infoLogin";
 
 function Main() {
      const {show, setShow} = useContext(ShowElements)
+     const {user, setUser} = useContext(InfoLogin)
+     const [typeRegister, setTypeRegister] = useState("register")
+
+     const login = () =>{
+          var Newname = document.getElementById('userName').value;
+          var password = document.getElementById('password').value;
+
+          if (password && Newname){
+               setUser({
+                    name:Newname,
+               })
+          }else{
+               alert("Please complete the login")
+          }
+     }
+
+     const register = () =>{
+          var Newname = document.getElementById('mail').value;
+          var password = document.getElementById('setPassword').value;
+          var confPassword = document.getElementById('confirmPassword').value;
+
+          if (password != confPassword){
+               alert("No coinciden las contraseñas")
+          }else{
+               if(Newname){
+                    
+                    alert("Fsdfds")
+
+               }else{
+                    alert("Please complete the Register") 
+               }
+          }
+
+
+     }
+
      return (
           <MainWrapper> 
                <h3>This is a main jeje</h3>
                <FormWrapper popup ={show}>
-                    <p>Crear Cuenta</p>
-                    <FormContainer>
+                    <OptionsContainer>
+                         <OptionsSelect onClick={()=>setTypeRegister("register")}>Register</OptionsSelect>
+                         <p>or</p>
+                         <OptionsSelect onClick={()=>setTypeRegister("login")}>Login</OptionsSelect>
+                    </OptionsContainer>
+                    <FormRegisterContainer show={typeRegister} id="FormRegisterContainer">
                          <DivForm>
                               <Label htmlFor={"mail"}>Mail</Label>
                               <Input type={"email"} id="mail" name="mail" />
                          </DivForm>
                          <DivForm>
-                              <Label htmlFor={"password"}>Password</Label>
-                              <Input type={"password"} id="password" name="password" />
+                              <Label htmlFor={"setPassword"}>Password</Label>
+                              <Input type={"password"} id="setPassword" name="setPassword" />
                          </DivForm>
                          <DivForm>
                               <Label htmlFor={"confirmPassword"}>Confirm Password</Label>
@@ -26,7 +67,20 @@ function Main() {
                               <Label htmlFor={"accept"}>Accept t&c</Label>
                               <Input type={"checkbox"} id="accept" name="accept" />
                          </DivForm>
-                    </FormContainer>
+                         <ButtonSendForm type="button" form="FormRegisterContainer" value={"Submit"} onClick={()=>register()}>Register</ButtonSendForm>
+                    </FormRegisterContainer>
+
+                    <FormLoginContariner id="FormLoginContariner" show={typeRegister}>
+                         <DivForm>
+                              <Label htmlFor={"userName"}>User</Label>
+                              <Input type={"userName"} id={"userName"} name="userName"/>
+                         </DivForm>
+                         <DivForm>
+                              <Label htmlFor={"password"}>Password</Label>
+                              <Input type={"password"} id="password" name="password" />
+                         </DivForm>
+                         <ButtonSendForm type="button" form="FormLoginContariner" value={"Submit"} onClick={()=>login()} >Login</ButtonSendForm>
+                    </FormLoginContariner>
                     
                </FormWrapper>
           </MainWrapper>
--- src/components/main/styles.js
+++ src/components/main/styles.js
@@ -24,14 +24,38 @@ export const FormWrapper = styled.div`
   padding: 1rem;
 `;
 
-export const FormContainer = styled.form`
-  display:flex;
+export const FormRegisterContainer = styled.form`
+  display:${props => props.show === "register" ? 'flex' : 'none'};
   flex-direction:column;
   justify-content:center;
+  margin-top:2rem;
+`;
+
+export const FormLoginContariner = styled.form`
+  display:${props=> props.show === "login" ? 'flex' : 'none'};
+  flex-direction:column;
+  justify-content:center;
+  margin-top:2rem;
+`;
+
+export const OptionsSelect = styled.p`
+  cursor: pointer ;
+  margin:0 1rem;
+  
+`;
+
+export const OptionsContainer = styled.div`
+  display:flex;
+  align-items:center;
 `;
 
 export const DivForm = styled.div`
-  margin: 10px 0;
+  margin:10px 10px;
+  display:flex;
+  flex-direction:column;
+  &:nth-child(4){
+    flex-direction:row;
+  }
 `;
 
 export const Input = styled.input`
@@ -39,6 +63,17 @@ export const Input = styled.input`
 `;
 
 export const Label = styled.label`
-  
+  margin-right:10px;
+`;
+
+export const ButtonSendForm = styled.button`
+  width:5rem;
+  height:3rem;
+  margin-top:10px;
+  align-self:center;
+`;
+
+export const P = styled.p`
+  cursor: pointer;
 `;
 
